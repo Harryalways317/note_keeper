691a9da123249722966dd90c8f8df99acb3efd40
Harish Vadaparty <harishvadaparty@gmail.com>
2021-05-24 21:08:50 +0530

    firestore data retrival implemented

---
+++ lib/database.dart
@@ -0,0 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+final CollectionReference _mainCollection = _firestore.collection('notes');
+
+class Database {
+  static String? userUid;
+
+// ...........................................
+  static Future<void> addItem({
+    required String title,
+    required String description,
+  }) async {
+    DocumentReference documentReferencer =
+        _mainCollection.doc(userUid).collection('items').doc();
+
+    Map<String, dynamic> data = <String, dynamic>{
+      "title": title,
+      "description": description,
+    };
+
+    await documentReferencer
+        .set(data)
+        .whenComplete(() => print("Notes item added to the database"))
+        .catchError((e) => print(e));
+  }
+
+// ...........................................
+  static Stream<QuerySnapshot> readItems() {
+    CollectionReference notesItemCollection =
+        _mainCollection.doc(userUid).collection('items');
+
+    return notesItemCollection.snapshots();
+  }
+}
